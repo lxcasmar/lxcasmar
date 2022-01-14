1e2144581cfd8cf2c51959eb56a6bca71768619a
Luis Castellanos <71403728+lxcasmar@users.noreply.github.com>
2022-01-13 22:20:16 -0500

    Update daysSinceCounter.py

--- daysSinceCounter.py
+++ daysSinceCounter.py
@@ -7,6 +7,7 @@ readme = open("README.md","r")
 lines = readme.readlines()
 today = date.today()
 worst_day = date(2014,6,29)
+# There's some weird formatting with the newline... just get it to work w readme file
 lines[-2] = (lines[-2][0:-5]+str((today-worst_day).days)+"\n")
 
 readme = open("README.md","w")
