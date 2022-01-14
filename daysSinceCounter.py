665e0ba0c57cd3803df15ceade16257324290bc0
Luis Castellanos <71403728+lxcasmar@users.noreply.github.com>
2022-01-13 22:12:53 -0500

    Update daysSinceCounter.py

--- daysSinceCounter.py
+++ daysSinceCounter.py
@@ -7,7 +7,7 @@ readme = open("README.md","r")
 lines = readme.readlines()
 today = date.today()
 worst_day = date(2014,6,29)
-lines[-2] = (lines[-2][0:-4]+str((today-worst_day).days))
+lines[-2] = (lines[-2][0:-5]+str((today-worst_day).days))
 
 readme = open("README.md","w")
 readme.writelines(lines)
